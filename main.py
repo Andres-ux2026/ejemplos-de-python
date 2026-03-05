5b074e7881489969ca83ca39e42a23c8eddd1983
Andres-ux2026 <moraga_2006@hotmail.com>
2026-03-05 01:09:26 +0000

    variables en python

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+print("hola mundo")
+
+#variables
+
+nombre= "andres"
+edad =19
+altura = 1.80
+es_profesor= False
+ciudad= "valparaiso"
+genero =["hombre","mujer","therian"] 
+
+#tipo de datos
+
+str # esto es un texto
+int # esto es un entero
+float # esto es un flotante
+bool # esto es un boleano
+
+
+texto= input()
+print(texto)
