c26409ab1aa62b39f3a531b86949ce153436adb4
Andres-ux2026 <moraga_2006@hotmail.com>
2026-03-14 00:51:06 +0000

    uso de funciones

---
+++ funciones.py
@@ -0,0 +1,12 @@
+""" def saludar():# se define una funcion
+    print("hola esta es mi primera funcion")
+
+saludar() #invocacion de la funcion """
+
+def sumar(numero1,numero2):
+    
+    suma=numero1+numero2
+    return suma
+
+mostrar = sumar(2,3)
+print(mostrar)
